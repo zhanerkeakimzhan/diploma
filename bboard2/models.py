434fbf842532d89d4385f2b834fb04e9bdfe2a94
zhanerkeakimzhan <74965537+Zhanerke2506@users.noreply.github.com>
2023-06-06 20:53:41 +0600

    template

--- bboard2/models.py
+++ bboard2/models.py
@@ -98,7 +98,7 @@ class Grade(models.Model):
     chairman = models.ForeignKey(Chairmans, on_delete=models.CASCADE, null=True, blank=True)
     student = models.ForeignKey(Students, on_delete=models.CASCADE)
     value = models.IntegerField()
-    question = models.CharField(max_length=100, default='')
+    question = models.CharField(max_length=1000, default='')
     is_filled = models.BooleanField(default=False)
 
     def __str__(self):
